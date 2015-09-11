41774c7311ae36db8aec4b0c207b9c83df4a7a4d
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-09-11 14:34:40 +0900

    Add compile test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,21 +1,27 @@
 require 'spec_helper'
 
 describe 'mackerel_agent' do
-  describe 'supporting operationg systems validation' do
-    context 'on RedHat' do
-      let(:facts) do
-        { :osfamily => 'RedHat' }
-      end
+  let(:facts) do
+    { :osfamily => 'RedHat' }
+  end
 
-      it { should contain_class('mackerel_agent') }
+  describe 'compile' do
+    it { should compile }
+    it { should compile.with_all_deps }
+    it { should contain_class('mackerel_agent') }
+  end
+
+  describe 'supporting operating systems validation' do
+    context 'on RedHat has been supported' do
+      it { should compile }
     end
 
-    context 'on Debian' do
+    context 'on Debian has been supported' do
       let(:facts) do
         { :osfamily => 'Debian' }
       end
 
-      it { should contain_class('mackerel_agent') }
+      it { should compile }
     end
   end
 end
