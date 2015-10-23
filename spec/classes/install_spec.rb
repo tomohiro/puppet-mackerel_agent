666a41e35a3d3e95ccb64683cf0868c7186f8aa3
suzupy <suzuki.n@pepabo.com>
2015-10-23 16:49:07 +0900

    test for /etc/mackerel-agent directory

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'mackerel_agent::install' do
   context 'with present (default)' do
     it { should contain_package('mackerel-agent').with_ensure('present') }
+    it { should contain_file('/etc/mackerel-agent').with_ensure('directory') }
     it { should contain_file('/etc/mackerel-agent/conf.d').with_ensure('directory') }
   end
 
