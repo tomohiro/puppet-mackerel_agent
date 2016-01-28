45b1cf9fbfe2029198eaa1371bf9319ec36cfca3
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-01-29 01:18:42 +0900

    Simplify options of install_dev_puppet_module

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,22 +1,18 @@
 require 'beaker-rspec'
 
+module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
 # Install Puppet agent on all hosts
 install_puppet_agent_on(hosts, {})
 
 RSpec.configure do |c|
-  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   c.formatter = :documentation
 
   c.before :suite do
-    # Install module to all hosts
-    hosts.each do |host|
-      install_dev_puppet_module(
-        source: module_root,
-        ignore_list: ['modules'],
-        module_name: 'mackerel_agent'
-      )
+    # Install mackerel_agent module to all hosts
+    install_dev_puppet_module(:source => module_root)
 
+    hosts.each do |host|
       # Install dependencies
       on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
       on(host, puppet('module', 'install', 'puppetlabs-apt'))
