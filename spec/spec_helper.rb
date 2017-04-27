b5de80af3b2e76eec1c101d3c4c9b5e27235677a
Kasumi Hanazuki <kasumi@rollingapple.net>
2017-04-27 19:31:15 +0900

    Define missing facts in classes spec
    Since v1.2.0 puppetlabs_spec_helper performs strict variable checking when used
    with Puppet 4. This patch defines facts used in the manifests to pass the specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.default_facts = { :osfamily => 'RedHat' }
+  c.default_facts = {
+    :architecture => 'amd64',
+    :osfamily => 'RedHat',
+    :operatingsystem => 'CentOS',
+  }
 end
